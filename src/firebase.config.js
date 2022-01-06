7f9eb6210a10bf8e90955d95a20bfd8f5f51800b
Sugata Mallick <you@example.com>
2022-01-06 18:19:07 +0600

    food

---
+++ src/firebase.config.js
@@ -0,0 +1,21 @@
+// console.log(process.env);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
+
+// export default firebaseConfig;
+const firebaseConfig = {
+  apiKey: "AIzaSyDtQHvoDA45KmhqQrMSQqJdczFDAAk0m18",
+  authDomain: "food-app-b3f3a.firebaseapp.com",
+  projectId: "food-app-b3f3a",
+  storageBucket: "food-app-b3f3a.appspot.com",
+  messagingSenderId: "1030480379576",
+  appId: "1:1030480379576:web:973ae27ba37d9bba6f7e4d",
+  measurementId: "G-3EEVERNP99"
+};
+export default firebaseConfig;
